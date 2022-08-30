6559b23d2abccce5d40371abda8e4bf64c3f7cf5
Aswinvaasits <84697530+Aswinvaasits@users.noreply.github.com>
2022-08-30 13:55:56 +0530

    updte

--- script.js
+++ script.js
@@ -69,5 +69,19 @@ var TxtRotate = function (el, toRotate, period) {
   }
 
 
+  // scroll
+
+  window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
   
