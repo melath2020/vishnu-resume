187c10ed50a26f48e2bfe5c82e4655937cab82b8
Aswinvaasits <84697530+Aswinvaasits@users.noreply.github.com>
2022-09-04 12:09:48 +0530

    update

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("navbar-header").style.top = "0";
   } else {
-    document.getElementById("navbar-header").style.top = "-200px";
+    document.getElementById("navbar-header").style.top = "-250px";
   }
 }
 
@@ -87,6 +87,9 @@ function scrollFunction() {
   var menu= document.querySelector('#nav-btn')
 var menuwrapper=document.querySelector('.mobile-nav-wrapper')
 var close= document.querySelector('#close-btn')
+var nav= document.querySelector('#nav-btns')
+var mobilenav=document.querySelector('.mobile-nav-wrappers')
+var remove= document.querySelector('#close-btns')
 
 menu.addEventListener('click',function(e){
     menuwrapper.style.display='block';
@@ -97,22 +100,26 @@ menu.addEventListener('click',function(e){
 close.addEventListener('click',function(e){
     menuwrapper.style.display='none';
     close.style.display='none';
-    menu.style.display='block'
+    menu.style.display='block';
+    mobilenav.style.display='none';
+    remove.style.display='none';
+    nav.style.display='block';
 })
 
 
-var nav= document.querySelector('#nav-btns')
-var mobilenav=document.querySelector('.mobile-nav-wrappers')
-var remove= document.querySelector('#close-btns')
+
 
 nav.addEventListener('click',function(e){
     mobilenav.style.display='block';
     remove.style.display='block';
     nav.style.display='none'
+    close.style.display='block';
+    menu.style.display='none';
 })
 
 remove.addEventListener('click',function(e){
     mobilenav.style.display='none';
     remove.style.display='none';
-    nav.style.display='block'
+    nav.style.display='block';
+    
 })
